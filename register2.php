14fb653aee114a710eb37ab7b04430523a931c8a
Akif1080 <87177071+Akif1080@users.noreply.github.com>
2021-09-06 02:52:31 +0600

    Add files via upload

---
+++ register2.php
@@ -0,0 +1,171 @@
+<?php
+session_start();
+include "db.php";
+if (isset($_POST["store_name"])) {
+
+	$store_name = $_POST["store_name"];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$repassword = $_POST['repassword'];
+	$mobile = $_POST['mobile'];
+	$address1 = $_POST['address1'];
+	$address2 = $_POST['address2'];
+	$name = "/^[a-zA-Z ]+$/";
+	$emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
+	$number = "/^[0-9]+$/";
+
+if(empty($store_name) || empty($email) || empty($password) || empty($repassword) ||
+	empty($mobile) || empty($address1) || empty($address2)){
+		
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>PLease Fill all fields..!</b>
+			</div>
+		";
+		exit();
+	} else {
+		if(!preg_match($name,$store_name)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>this $f_name is not valid..!</b>
+			</div>
+		";
+		exit();
+	}
+	
+	if(!preg_match($emailValidation,$email)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>this $email is not valid..!</b>
+			</div>
+		";
+		exit();
+	}
+	if(strlen($password) < 2 ){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Password is weak! Must contain at least 8 characters</b>
+			</div>
+		";
+		exit();
+	}
+	
+	if($password != $repassword){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>password is not same</b>
+			</div>
+		";
+	}
+	if(!preg_match($number,$mobile)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Mobile number $mobile is not valid</b>
+			</div>
+		";
+		exit();
+	}
+	if(!(strlen($mobile) == 2)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Mobile number must be 10 digit</b>
+			</div>
+		";
+		exit();
+	}
+	//existing email address in our database
+	$sql = "SELECT user_id FROM new_users WHERE email = '$email' LIMIT 1" ;
+	$check_query = mysqli_query($con,$sql);
+	$count_email = mysqli_num_rows($check_query);
+	if($count_email > 0){
+		echo "
+			<div class='alert alert-danger'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Email Address is already available! Try Another email address</b>
+			</div>
+		";
+		exit();
+	} else {
+		
+		$sql = "INSERT INTO `new_users`
+		(`user_id`, `store_name`, `last_name`, `email`,
+		`password`, `mobile`, `address1`, `address2`) 
+		VALUES (NULL, '$store_name', '$email', 
+		'$password', '$mobile', '$address1', '$address2')";
+		$run_query = mysqli_query($con,$sql);
+		$_SESSION["uid"] = mysqli_insert_id($con);
+		$_SESSION["name"] = $store_name;
+		$ip_add = getenv("REMOTE_ADDR");
+		$sql = "UPDATE cart SET user_id = '$_SESSION[uid]' WHERE ip_add='$ip_add' AND user_id = -1";
+		if(mysqli_query($con,$sql)){
+			echo "register_success";
+			echo "<script> location.href='checkout.php'; </script>";
+            exit;
+		}
+	}
+	}
+
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
